043ea6e9eb48fe6ed74d78e90ba9d4d7daebda3e
SinghKaranveer <ks3379@drexel.edu>
2019-08-24 00:22:58 -0400

    Finished layers function

--- main.py
+++ main.py
@@ -26,7 +26,40 @@ def load_vgg(sess, path):
     layer4 = graph.get_tensor_by_name('layer4_out:0')
     layer7 = graph.get_tensor_by_name('layer7_out:0')
     return input_layer, keep_prob, layer3, layer4, layer7
+
+def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+    """
+    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
+    :param vgg_layer3_out: TF Tensor for VGG Layer 3 output
+    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
+    :param vgg_layer7_out: TF Tensor for VGG Layer 7 output
+    :param num_classes: Number of classes to classify
+    :return: The Tensor for the last layer of output
+    """
+    fcn8 = tf.layers.conv2d(vgg_layer7_out, filters=num_classes, kernel_size=1, name='fcn8')
     
+    fcn9 = tf.layers.conv2d_transpose(fcn8, filters=vgg_layer4_out.get_shape().as_list()[-1], kernel_size=4, strides=(2, 2), padding='SAME', name='fcn9')
+    
+    fcn9_skip = tf.add(fcn9, vgg_layer4_out, name='fcn9_skip')
+
+    fcn10 = tf.layers.conv2d_transpose(fcn9_skip, filters=vgg_layer3_out.get_shape().as_list()[-1], kernel_size=4, strides=(2, 2), padding='SAME', name='fcn10')
+
+    fcn10_skip = tf.add(fcn10, vgg_layer4_out, name='fcn10_skip')
+
+    fcn11 = tf.layers.conv2d_transpose(fcn10_skip, filters=num_classes, kernel_size=16, strides=(8, 8), padding='SAME', name="fcn11")
+    
+    return fcn11
+
+def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
+    """
+    Build the TensorFlow loss and optimizer operations.
+    :param nn_last_layer: TF Tensor of the last layer in the neural network
+    :param correct_label: TF Placeholder for the correct label image
+    :param learning_rate: TF Placeholder for the learning rate
+    :param num_classes: Number of classes to classify
+    :return: Tuple of (logits, train_op, cross_entropy_loss)
+    """
+    return None, None, None
 
 if __name__ == "__main__":
     data_dir = './data'
