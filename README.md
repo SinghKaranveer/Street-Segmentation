dd6aeffdbea37797a70e1a3abae989a4fa44dcb6
SinghKaranveer <ks3379@drexel.edu>
2019-08-24 23:04:35 -0400

    70 epoch 0.408 Loss

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Street-Segmentation
-This project is from Udacity's Self-Driving Car curriculum.  
+This project is from Udacity's Self-Driving Car curriculum.  They provided the helper functions as well as a canned VGG network with fully connected layers replaced with 1x1 convolutional layers.  
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import helper
 vgg_path = './data/vgg'
 num_classes = 2
 image_shape = (160, 576)
-EPOCHS = 40
+EPOCHS = 70
 BATCH_SIZE = 16
 DROPOUT = 0.75
 
@@ -24,74 +24,64 @@ else:
 
 def load_vgg(sess, path):
     model = tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
+
     graph = tf.get_default_graph()
+
     input_layer = graph.get_tensor_by_name('image_input:0')
+
     keep_prob = graph.get_tensor_by_name('keep_prob:0')
+
     layer3 = graph.get_tensor_by_name('layer3_out:0')
+
     layer4 = graph.get_tensor_by_name('layer4_out:0')
+
     layer7 = graph.get_tensor_by_name('layer7_out:0')
+
     return input_layer, keep_prob, layer3, layer4, layer7
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
-    """
-    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
-    :param vgg_layer3_out: TF Tensor for VGG Layer 3 output
-    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
-    :param vgg_layer7_out: TF Tensor for VGG Layer 7 output
-    :param num_classes: Number of classes to classify
-    :return: The Tensor for the last layer of output
-    """
-    fcn8 = tf.layers.conv2d(vgg_layer7_out, filters=num_classes, kernel_size=1, name='fcn8')
-    
-    fcn9 = tf.layers.conv2d_transpose(fcn8, filters=vgg_layer4_out.get_shape().as_list()[-1], kernel_size=4, strides=(2, 2), padding='SAME', name='fcn9')
-    
-    fcn9_skip = tf.add(fcn9, vgg_layer4_out, name='fcn9_skip')
 
-    fcn10 = tf.layers.conv2d_transpose(fcn9_skip, filters=vgg_layer3_out.get_shape().as_list()[-1], kernel_size=4, strides=(2, 2), padding='SAME', name='fcn10')
+    layer3, layer4, layer7 = vgg_layer3_out, vgg_layer4_out, vgg_layer7_out
 
-    fcn10_skip = tf.add(fcn10, vgg_layer3_out, name='fcn10_skip')
+    fcn8 = tf.layers.conv2d(layer7, filters=num_classes, kernel_size=1, name="fcn8")
 
-    fcn11 = tf.layers.conv2d_transpose(fcn10_skip, filters=num_classes, kernel_size=16, strides=(8, 8), padding='SAME', name="fcn11")
-    
+    fcn9 = tf.layers.conv2d_transpose(fcn8, filters=layer4.get_shape().as_list()[-1],
+    kernel_size=4, strides=(2, 2), padding='SAME', name="fcn9")
+
+    fcn9_skip_connected = tf.add(fcn9, layer4, name="fcn9_plus_vgg_layer4")
+
+    fcn10 = tf.layers.conv2d_transpose(fcn9_skip_connected, filters=layer3.get_shape().as_list()[-1],
+    kernel_size=4, strides=(2, 2), padding='SAME', name="fcn10_conv2d")
+
+    fcn10_skip_connected = tf.add(fcn10, layer3, name="fcn10_plus_vgg_layer3")
+
+    fcn11 = tf.layers.conv2d_transpose(fcn10_skip_connected, filters=num_classes,
+    kernel_size=16, strides=(8, 8), padding='SAME', name="fcn11")
+
     return fcn11
 
+
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
-    """
-    Build the TensorFlow loss and optimizer operations.
-    :param nn_last_layer: TF Tensor of the last layer in the neural network
-    :param correct_label: TF Placeholder for the correct label image
-    :param learning_rate: TF Placeholder for the learning rate
-    :param num_classes: Number of classes to classify
-    :return: Tuple of (logits, train_op, cross_entropy_loss)
-    """
     output_reshaped = tf.reshape(nn_last_layer, (-1, num_classes), name="output_reshaped")
+
     label_reshaped = tf.reshape(correct_label, (-1, num_classes), name="label_reshaped")
+
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=output_reshaped, labels=label_reshaped[:])
+
     loss = tf.reduce_mean(cross_entropy, name="loss")
+
     train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, name="train")
 
     return output_reshaped, train, loss
 
+
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate):
-    """
-    Train neural network and print out the loss during training.
-    :param sess: TF Session
-    :param epochs: Number of epochs
-    :param batch_size: Batch size
-    :param get_batches_fn: Function to get batches of training data.  Call using get_batches_fn(batch_size)
-    :param train_op: TF Operation to train the neural network
-    :param cross_entropy_loss: TF Tensor for the amount of loss
-    :param input_image: TF Placeholder for input images
-    :param correct_label: TF Placeholder for label images
-    :param keep_prob: TF Placeholder for dropout keep probability
-    :param learning_rate: TF Placeholder for learning rate
-    """
     for epoch in range(epochs):
         total_loss = 0
         for x, y in get_batches_fn(batch_size):
-            loss = sess.run(cross_entropy_loss, 
-            feed_dict={input_image: x, correct_label: y, keep_prob: 0.5, learning_rate: 0.001})
+            loss, _ = sess.run([cross_entropy_loss, train_op], 
+            feed_dict={input_image: x, correct_label: y, keep_prob: 0.75, learning_rate: 0.0001})
 
             total_loss += loss
 
@@ -109,10 +99,6 @@ def run():
 
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
-
-    # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
-    # You'll need a GPU with at least 10 teraFLOPS to train on.
-    #  https://www.cityscapes-dataset.com/
 
     with tf.Session() as sess:
         vgg_path = os.path.join(data_dir, 'vgg')
